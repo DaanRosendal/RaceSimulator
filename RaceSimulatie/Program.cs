5451028fc245dc073720deed86d44be69914ec4c
Daan Rosendal <daan.rosendal@unive.nl>
2021-09-13 18:08:11 +0200

    first commit

---
+++ RaceSimulatie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RaceSimulatie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
